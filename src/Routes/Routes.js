12c612a96fffdbc071cf39fe37253abe2c6d64c9
luizgustavodiniz <gustavo.diniz.gon@gmail.com>
2023-03-30 09:44:05 -0300

    criaçao da routes

---
+++ src/Routes/Routes.js
@@ -0,0 +1,241 @@
+import React from 'react';
+import { View, SafeAreaView, StyleSheet, Text, ScrollView, Image, TouchableOpacity } from 'react-native';
+
+export default function Routes() {
+ return (
+   <SafeAreaView style={styles.container}>
+      <View>
+        <Text>Lanches NERD</Text>
+      </View>
+      <View>
+        <Text style={{fontSize: 22, color: '#141414', marginTop: 15}}>Promoções</Text>
+      </View>
+      <View>
+
+      
+      <ScrollView horizontal={true}>
+        
+        <View style={styles.promoItemArea}>
+          <View style={{flexDirection: 'row', alignItems: 'center', justifyContent: 'center', margin: 10}}>
+            <Text style={[styles.promoItemText, {color: '#141414', fontWeight: 'bold'}]}>X-Burguer</Text>
+
+              
+          </View>
+          <View style={{margin: 10, alignItems: 'center', justifyContent: 'center', marginBottom: 25}}>
+                <Text style={{color: '#141414'}}>2 X-burguer</Text>
+                <Text style={{color: '#141414'}}>Para duas pessoas</Text>
+          </View>
+          <View>
+          <View style={{backgroundColor: '#DB0006', alignSelf: 'flex-end', width: 70, height: 70, borderRadius: 50, alignItems: 'center', justifyContent: 'center', marginBottom: -90, marginRight: 40, zIndex: 10}}>
+            <Text style={{marginLeft: 8, fontSize: 20, fontStyle: 'italic', fontWeight: 'bold', color: '#FFF'}}>-20%</Text>
+          </View>
+          <Image
+             source={{uri: 'https://img.freepik.com/fotos-gratis/hamburguer-de-vista-frontal-em-um-carrinho_141793-15542.jpg?size=626&ext=jpg'}}
+             style={styles.promoPhotos}
+            />
+         
+          
+          </View>
+
+          
+          
+          <View>
+          <TouchableOpacity style={styles.promoItemBtn}>
+              <Text>Ver Mais </Text>
+            </TouchableOpacity>
+          </View>
+            
+            
+        </View>
+            
+      
+
+        <View style={styles.promoItemArea}>
+          <View style={{flexDirection: 'row', alignItems: 'center', justifyContent: 'center', margin: 10}}>
+            <Text style={[styles.promoItemText, {color: '#141414', fontWeight: 'bold'}]}>X-Burguer</Text>
+
+              
+          </View>
+          <View style={{margin: 10, alignItems: 'center', justifyContent: 'center', marginBottom: 25}}>
+                <Text style={{color: '#141414'}}>2 X-burguer</Text>
+                <Text style={{color: '#141414'}}>Para duas pessoas</Text>
+          </View>
+          <View>
+          <View style={{backgroundColor: '#DB0006', alignSelf: 'flex-end', width: 70, height: 70, borderRadius: 50, alignItems: 'center', justifyContent: 'center', marginBottom: -90, marginRight: 40, zIndex: 10}}>
+            <Text style={{marginLeft: 8, fontSize: 20, fontStyle: 'italic', fontWeight: 'bold', color: '#FFF'}}>-20%</Text>
+          </View>
+          <Image
+             source={{uri: 'https://img.freepik.com/fotos-gratis/hamburguer-de-vista-frontal-em-um-carrinho_141793-15542.jpg?size=626&ext=jpg'}}
+             style={styles.promoPhotos}
+            />
+         
+          
+          </View>
+
+          
+          
+          <View>
+          <TouchableOpacity style={styles.promoItemBtn}>
+              <Text>Ver Mais </Text>
+            </TouchableOpacity>
+          </View>
+            
+            
+        </View>
+
+        <View style={styles.promoItemArea}>
+          <View style={{flexDirection: 'row', alignItems: 'center', justifyContent: 'center', margin: 10}}>
+            <Text style={[styles.promoItemText, {color: '#141414', fontWeight: 'bold'}]}>X-Burguer</Text>
+
+              
+          </View>
+          <View style={{margin: 10, alignItems: 'center', justifyContent: 'center', marginBottom: 25}}>
+                <Text style={{color: '#141414'}}>2 X-burguer</Text>
+                <Text style={{color: '#141414'}}>Para duas pessoas</Text>
+          </View>
+          <View>
+          <View style={{backgroundColor: '#DB0006', alignSelf: 'flex-end', width: 70, height: 70, borderRadius: 50, alignItems: 'center', justifyContent: 'center', marginBottom: -90, marginRight: 40, zIndex: 10}}>
+            <Text style={{marginLeft: 8, fontSize: 20, fontStyle: 'italic', fontWeight: 'bold', color: '#FFF'}}>-20%</Text>
+          </View>
+          <Image
+             source={{uri: 'https://img.freepik.com/fotos-gratis/hamburguer-de-vista-frontal-em-um-carrinho_141793-15542.jpg?size=626&ext=jpg'}}
+             style={styles.promoPhotos}
+            />
+         
+          
+          </View>
+
+          
+          
+          <View>
+          <TouchableOpacity style={styles.promoItemBtn}>
+              <Text>Ver Mais </Text>
+            </TouchableOpacity>
+          </View>
+            
+            
+        </View>
+
+        <View style={styles.promoItemArea}>
+          <View style={{flexDirection: 'row', alignItems: 'center', justifyContent: 'center', margin: 10}}>
+            <Text style={[styles.promoItemText, {color: '#141414', fontWeight: 'bold'}]}>X-Burguer</Text>
+
+              
+          </View>
+          <View style={{margin: 10, alignItems: 'center', justifyContent: 'center', marginBottom: 25}}>
+                <Text style={{color: '#141414'}}>2 X-burguer</Text>
+                <Text style={{color: '#141414'}}>Para duas pessoas</Text>
+          </View>
+          <View>
+          <View style={{backgroundColor: '#DB0006', alignSelf: 'flex-end', width: 70, height: 70, borderRadius: 50, alignItems: 'center', justifyContent: 'center', marginBottom: -90, marginRight: 40, zIndex: 10}}>
+            <Text style={{marginLeft: 8, fontSize: 20, fontStyle: 'italic', fontWeight: 'bold', color: '#FFF'}}>-20%</Text>
+          </View>
+          <Image
+             source={{uri: 'https://img.freepik.com/fotos-gratis/hamburguer-de-vista-frontal-em-um-carrinho_141793-15542.jpg?size=626&ext=jpg'}}
+             style={styles.promoPhotos}
+            />
+         
+          
+          </View>
+
+          
+          
+          <View>
+          <TouchableOpacity style={styles.promoItemBtn}>
+              <Text>Ver Mais </Text>
+            </TouchableOpacity>
+          </View>
+            
+            
+        </View>
+
+        <View style={styles.promoItemArea}>
+          <View style={{flexDirection: 'row', alignItems: 'center', justifyContent: 'center', margin: 10}}>
+            <Text style={[styles.promoItemText, {color: '#141414', fontWeight: 'bold'}]}>X-Burguer</Text>
+
+              
+          </View>
+          <View style={{margin: 10, alignItems: 'center', justifyContent: 'center', marginBottom: 25}}>
+                <Text style={{color: '#141414'}}>2 X-burguer</Text>
+                <Text style={{color: '#141414'}}>Para duas pessoas</Text>
+          </View>
+          <View>
+          <View style={{backgroundColor: '#DB0006', alignSelf: 'flex-end', width: 70, height: 70, borderRadius: 50, alignItems: 'center', justifyContent: 'center', marginBottom: -90, marginRight: 40, zIndex: 10}}>
+            <Text style={{marginLeft: 8, fontSize: 20, fontStyle: 'italic', fontWeight: 'bold', color: '#FFF'}}>-20%</Text>
+          </View>
+          <Image
+             source={{uri: 'https://img.freepik.com/fotos-gratis/hamburguer-de-vista-frontal-em-um-carrinho_141793-15542.jpg?size=626&ext=jpg'}}
+             style={styles.promoPhotos}
+            />
+         
+          
+          </View>
+
+          
+          
+          <View>
+          <TouchableOpacity style={styles.promoItemBtn}>
+              <Text>Ver Mais </Text>
+            </TouchableOpacity>
+          </View>
+            
+            
+        </View>
+        
+      </ScrollView>
+
+      </View>
+   </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#F5F5F5'
+    },
+    promoList: {
+      flexDirection: 'column'
+    },
+    promoItemArea: {
+      backgroundColor: '#FFFAFA',
+      margin: 10,
+      width: 250,      
+      shadowColor: "#000",
+
+      shadowOffset: {
+        width: 0,
+        height: 3,
+       },
+       shadowOpacity: 0.27,
+       shadowRadius: 4.65,
+  
+    elevation: 6
+    },
+    promoItemText: {
+      fontSize: 20,
+    },
+    promoPhotos: {
+       width: 150,
+       height: 150,
+       margin: 9,
+       borderRadius: 80,
+       alignSelf: 'center'
+    },
+    promoItemBtn: {
+      backgroundColor: '#FFBC0C',
+      width: 100,
+      height: 40,
+      alignSelf: 'center',
+      alignItems: 'center',
+      justifyContent: 'center',
+      margin: 8,
+      borderRadius: 10
+    },
+    promoItemText:{
+      fontSize: 20
+    },
+    scrollArea: {
+      
+    }
+});
